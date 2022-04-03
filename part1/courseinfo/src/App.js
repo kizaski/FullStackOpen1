cade308f5255e2ec328645d39920b10189abcfeb
Kiril <kiril2315@gmail.com>
2022-04-04 01:39:56 +0300

    1.1: course information, step1
    Separate in components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,27 +1,64 @@
 const App = () =>
 {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  let exercises_sum = 0
+  for ( const val of parts.filter( x => x.exercises ) )
+  {
+    exercises_sum += val.exercises
+  }
+
+  let content = []
+  for ( let i = 0; i < parts.length; i++ )
+  {
+    content.push(
+      <Content name={ parts.at( i ).name } exercises={ parts.at( i ).exercises } />
+    )
+  }
 
   return (
     <div>
-      <h1>{ course }</h1>
-      <p>
-        { part1 } { exercises1 }
-      </p>
-      <p>
-        { part2 } { exercises2 }
-      </p>
-      <p>
-        { part3 } { exercises3 }
-      </p>
-      <p>Number of exercises { exercises1 + exercises2 + exercises3 }</p>
-    </div>
+      <Header title={ course } />
+      { content }
+      <TotalExercises sum={ exercises_sum } />
+    </div >
+  )
+}
+
+const TotalExercises = ( props ) =>
+{
+  return (
+    <p>Number of exercises { props.sum }</p>
+  )
+}
+
+const Content = ( props ) =>
+{
+  return (
+    <p>
+      { props.name } { props.exercises }
+    </p>
+  )
+}
+
+const Header = ( props ) =>
+{
+  return (
+    <h1>{ props.title }</h1>
   )
 }
 
