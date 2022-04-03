c3b3bc7d958dcde89dc7ae5660cf1193eeacaa79
Kiril <kiril2315@gmail.com>
2022-04-03 23:27:01 +0300

    init

---
+++ part1/test-react/src/App.js
@@ -0,0 +1,25 @@
+const Hello = ( props ) =>
+{
+  return (
+    <div>
+      <p>
+        Hello { props.name }, you are { props.age } years old
+      </p>
+    </div>
+  )
+}
+const App = () =>
+{
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={ 26 + 10 } />
+      <Hello name={ name } age={ age } />
+    </>
+  )
+}
+
+export default App
