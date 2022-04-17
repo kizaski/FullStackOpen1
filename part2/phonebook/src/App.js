a8fb41a7898e9ff9ad51102dcc0c3cad054384d8
Kiril <kiril2315@gmail.com>
2022-04-17 17:09:49 +0300

    part2: 2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,33 @@
 import { useState, useEffect } from 'react'
-//import componentname from workingdirectory
+import axios from 'axios'
+//todo import componentname from workingdirectory
 
 const App = () =>
 {
+  /*
   const [ persons, setPersons ] = useState( [
     { name: 'Arto Hellas', number: '999-000-000', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ] )
-
+  */
+  const [ persons, setPersons ] = useState( [] )
   const [ filter, setFilter ] = useState( '' )
+
+  useEffect( () =>
+  {
+    console.log( 'effect' )
+
+    axios.get( 'http://localhost:3001/db' ).then( response =>
+    {
+      console.log( 'promise fulfilled' )
+      setPersons( response.data.persons )
+    } )
+  }, [] )
+
+  console.log( 'render', persons.length, 'notes' )
+
 
   return (
     <div>
